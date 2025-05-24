38481eaea7cb0e90e062f36d32ef7565aaba2c0a
yinanwy <2977713400@qq.com>
2025-05-25 01:47:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import shap
-import matplotlib.pyplot as plt  
-
+import matplotlib.pyplot as plt
 @st.cache_resource
 def load_resources():
     # 加载8维预处理器和模型
